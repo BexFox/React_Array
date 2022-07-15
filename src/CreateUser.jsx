64337b0ab0250736b4d1d3a32d58b76bda629faa
BexFox <karis601293@gmail.com>
2022-07-15 17:00:50 +0900

    1.Add func of focusing by using onKeyDown

--- src/CreateUser.jsx
+++ src/CreateUser.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function CreateUser({ username, email, onChange, onCreate }) {
+  const firstInput = useRef();
+  const secondInput = useRef();
+
   return (
     <div>
       <input
+        autoFocus
         name='username'
         placeholder='ID'
         onChange={onChange}
         value={username}
+        ref={firstInput}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            secondInput.current.focus();
+          }
+        }}
       />
       <input
         name='email'
         placeholder='Email'
         onChange={onChange}
         value={email}
+        ref={secondInput}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onCreate();
+            firstInput.current.focus();
+          }
+        }}
       />
       <button onClick={onCreate}>Register</button>
     </div>
