aa6a3eb9511bd921d914f773759dd6df21ed9f4e
BexFox <karis601293@gmail.com>
2022-07-16 12:56:44 +0900

    Add fucn of Editting in array

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './UserList.css';
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div className='userList'>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black',
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>{' '}
+      <span>({user.email})</span>
       <button
         className='del_Btn'
         onClick={() => {
@@ -16,11 +25,16 @@ function User({ user, onRemove }) {
   );
 }
 
-export default function UserList({ users, onRemove }) {
+export default function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
